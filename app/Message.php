61fcc61cb816eee4aa87e304cccfe14ff39e7a8c
NelsonRodriguez <nelsonmatul2013@gmail.com>
2020-07-03 16:24:08 -0600

    **FEAT** Se añade el readme para la instalación del proyecto y también las modificaciones para el modelo de la tabla de mensajes

--- app/Message.php
+++ app/Message.php
@@ -3,23 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-
 
 class Message extends Model
 {
-    //
-    public function up()
-    {
-    	Schema::create('message', function (Blueprint $table){
-    		$table->increments('id');
-    		$table->timestamps();
-    	});
-    }
-
-    public function down()
-    {
-        Schema::dropIfExists('message');
-    }
+    protected $fillable = ['tag', 'msj'];
 }
